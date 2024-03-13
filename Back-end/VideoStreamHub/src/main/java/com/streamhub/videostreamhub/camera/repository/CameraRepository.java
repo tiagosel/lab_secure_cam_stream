b18db34a93a7b3a2a77082a6a040ba05f1d18ce8
Tiago Neves Borges <tiagosel@gmail.com>
2024-03-13 12:49:10 -0300

    feat(us002): Add functionality for listing cameras
    - Added functionality to list cameras with specified information
    - Implemented sorting by Customer ID in ascending order
    - Implemented pagination with 10 records per page
    
    #US002

--- Back-end/VideoStreamHub/src/main/java/com/streamhub/videostreamhub/camera/repository/CameraRepository.java
+++ Back-end/VideoStreamHub/src/main/java/com/streamhub/videostreamhub/camera/repository/CameraRepository.java
@@ -1,6 +1,10 @@
 package com.streamhub.videostreamhub.camera.repository;
 
+import io.micrometer.observation.ObservationFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CameraRepository extends JpaRepository<Camera, Long> {
+    Page<Camera> findAllByCustomerIdEquals(Pageable page, Long id);
 }
